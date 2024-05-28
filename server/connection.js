f46f0bbe0da91306afa00d0b559c2e304ecfc753
912-Dumitrescu-David <davidioan.dumitrescu@gmail.com>
2024-05-28 19:22:19 +0300

    db final

--- server/connection.js
+++ server/connection.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
     host: 'database-1.c98m6ssew1p7.eu-north-1.rds.amazonaws.com',
     user: 'admin',
     password: 'davidmpp',
-    database: 'database-1'
+    database: 'mpp'
 });
 
 module.exports = connection;
