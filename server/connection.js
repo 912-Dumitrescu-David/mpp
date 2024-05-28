95ddac16b5ba5bdfba494a0b36d0df797372d5fa
912-Dumitrescu-David <davidioan.dumitrescu@gmail.com>
2024-05-28 18:49:32 +0300

    aws db?

--- server/connection.js
+++ server/connection.js
@@ -1,9 +1,17 @@
 const mysql = require('mysql');
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'david',
+//     database: 'mpp'
+// });
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'david',
-    database: 'mpp'
+    host: 'database-1.c98m6ssew1p7.eu-north-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'davidmpp',
+    database: 'database-1'
 });
+
 module.exports = connection;
